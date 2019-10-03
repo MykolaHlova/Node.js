a8b15b78915c09f6e4b10c9c81c9bc8f4b58a85d
MykolaHlova <kolyahlova@gmail.com>
2019-10-03 11:35:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const createStudens = require('./createStudent');
+const addPhoto = require('./addPhoto');
+
+
+fs.mkdir(`./jscx-1800`, (err) => {
+
+    if(err) console.log(err);
+});
+
+fs.mkdir(`./jscx-2000`, (err) => {
+
+    if (err) console.log(err);
+});
+
+createStudens('Ivan_Ivan', 'jscx-1800', 'age: 27');
+createStudens('Vitalik_Vitalik','jscx-2000','age:25');
+
+addPhoto('./jscx-1800/Ivan_Ivan/photo.jpg','../1.jpg');
+addPhoto('./jscx-2000/Vitalik_Vitalik/photo.jpg','../2.jpg');
